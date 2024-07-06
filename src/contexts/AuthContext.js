a3b7d763e85e89cee15fdc495bc15ac5171de9b9
karthik <wolfenbitzyt@gmail.com>
2024-07-07 02:58:41 +0530

    user state and fetchuser function in auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,27 +1,49 @@
 'use client'
+import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [isAuthenicated, setIsAuthenicated] = useState(false);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         const storedToken = localStorage.getItem('token');
-        setIsAuthenicated(!!storedToken);
+        if(storedToken){
+            fetchUserData();
+        }
     }, []);
 
+    const fetchUserData = async () => {
+        try{
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/profile`, {
+                withCredentials : true
+            })
+            setIsAuthenicated(true);
+            setUser(res.data.data);
+        } catch(error){
+            console.log("Eroor fetching user :", error);
+            setIsAuthenicated(false);
+            setUser(null);
+        }
+    }
+
     const login = () => {
-        setIsAuthenicated(true);
+        const storedToken = localStorage.getItem('token');
+        if(storedToken){
+            fetchUserData();
+        }
     }
 
     const logout = () => {
         localStorage.removeItem('token');
         setIsAuthenicated(false);
+        setUser(null);
     }
 
     return (
-        <AuthContext.Provider value = {{isAuthenicated, login, logout}}>
+        <AuthContext.Provider value = {{isAuthenicated, user, login, logout, fetchUserData}}>
             {children}
         </AuthContext.Provider>
     )
