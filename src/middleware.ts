58bec2f6d7e27892cd2f02c4b667e760247a648e
karthik <wolfenbitzyt@gmail.com>
2024-07-07 01:39:28 +0530

    next middleware added to protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import type { NextRequest } from "next/server";
+
+export function middleware(request : NextRequest) {
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === '/sign-in' || path === '/sign-up'
+
+    const token = request.cookies.get('token')?.value || '';
+    if(isPublicPath && token){
+        return Response.redirect(new URL('/dashboard', request.url));
+    }
+
+    if(!isPublicPath && !token){
+        return Response.redirect(new URL('/sign-in', request.url));
+    }
+}
+
+export const config = {
+    matcher: [
+        "/sign-in",
+        "/sign-up",
+    ]
+}
